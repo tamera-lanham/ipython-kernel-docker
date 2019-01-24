e052373b7a1476f188816eefd8b75bec7ed2caf9
Lanham <lanhamt@ELSPHIM-221451.science.regn.net>
2019-01-24 16:55:35 -0500

    Fix the entrypoint to use default connection file when not specified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-CONNECTION_FILE=$1
+if [ $# -lt 1 ]; then
+    connection_file=/tmp/connection-file.json
+else
+    connection_file=$1
+fi
 
-# Get port numbers from the connection_file
-CONTROL_PORT=$(cat $CONNECTION_FILE | jq '.control_port')
-SHELL_PORT=$(cat $CONNECTION_FILE | jq '.shell_port')
-STDIN_PORT=$(cat $CONNECTION_FILE | jq '.stdin_port')
-HB_PORT=$(cat $CONNECTION_FILE | jq '.hb_port')
-IOPUB_PORT=$(cat $CONNECTION_FILE | jq '.iopub_port')
 
-echo "--hb=$HB_PORT --shell=$SHELL_PORT --iopub=$IOPUB_PORT --stdin=$STDIN_PORT --control=$CONTROL_PORT"
+# Get the port number out
+# Here we assume the local machine has JQ installed
+CONTROL_PORT=$(jq '.control_port' < $connection_file)
+SHELL_PORT=$(jq '.shell_port' < $connection_file)
+STDIN_PORT=$(jq '.stdin_port' < $connection_file)
+HB_PORT=$(jq '.hb_port' < $connection_file)
+IOPUB_PORT=$(jq '.iopub_port' < $connection_file)
+
+echo control ${CONTROL_PORT}
+echo shell ${SHELL_PORT}
+echo stdin ${STDIN_PORT}
+echo hb ${HB_PORT}
+echo iopub ${IOPUB_PORT}
 
 python3 -m ipykernel_launcher --ip=0.0.0.0 --hb=$HB_PORT --shell=$SHELL_PORT --iopub=$IOPUB_PORT --stdin=$STDIN_PORT --control=$CONTROL_PORT -f $CONNECTION_FILE
