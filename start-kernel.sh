66a0e3c8146baf8bd8b39f5b4e24efe243f4d013
Lanham <lanhamt@ELSPHIM-221451.science.regn.net>
2019-01-24 16:44:36 -0500

    Modified start kernel

--- start-kernel.sh
+++ start-kernel.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-CONNECTION_FILE=$1
+if [ $# -lt 1 ]; then
+    echo "Missing the connection file as first parameter"
+    exit 1
+fi
 
-# Get port numbers from the connection_file
-CONTROL_PORT=$(cat $CONNECTION_FILE | jq '.control_port')
-SHELL_PORT=$(cat $CONNECTION_FILE | jq '.shell_port')
-STDIN_PORT=$(cat $CONNECTION_FILE | jq '.stdin_port')
-HB_PORT=$(cat $CONNECTION_FILE | jq '.hb_port')
-IOPUB_PORT=$(cat $CONNECTION_FILE | jq '.iopub_port')
+# Script settings
+DOCKER_IMAGE=python-kernel
 
-echo "Got ports"
-echo "--hb=$HB_PORT --shell=$SHELL_PORT --iopub=$IOPUB_PORT --stdin=$STDIN_PORT --control=$CONTROL_PORT"
+connection_file=$1
+
+# Get the port number out
+# Here we assume the local machine has JQ installed
+CONTROL_PORT=$(jq '.control_port' < $connection_file)
+SHELL_PORT=$(jq '.shell_port' < $connection_file)
+STDIN_PORT=$(jq '.stdin_port' < $connection_file)
+HB_PORT=$(jq '.hb_port' < $connection_file)
+IOPUB_PORT=$(jq '.iopub_port' < $connection_file)
 
 # Run docker image with the connection file mounted in, and ports forwarded
-docker run -d -v $(dirname $CONNECTION_FILE):/tmp \
+docker run -d --rm \
+    -v $connection_file:/tmp/connection-file.json:ro \
     -p $CONTROL_PORT:$CONTROL_PORT \
     -p $SHELL_PORT:$SHELL_PORT \
     -p $STDIN_PORT:$STDIN_PORT \
     -p $HB_PORT:$HB_PORT \
     -p $IOPUB_PORT:$IOPUB_PORT \
-    -t python-kernel:latest \
-    /tmp/$(basename $CONNECTION_FILE)
+    $DOCKER_IMAGE
