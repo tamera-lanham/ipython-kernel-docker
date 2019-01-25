1543351302c50c76a18ca4495df239b805e30b60
Lanham <lanhamt@ELSPHIM-221451.science.regn.net>
2019-01-25 14:35:16 -0500

    Another stab at this by Tan

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ else
 fi
 
 cat $connection_file
+# Modify the damn connection file to use proper IP address now
+sed -i 's;127.0.0.1;0.0.0.0;' ${connection_file}
 
-python3 -m ipykernel_launcher -f $connection_file
+python3 -m ipykernel_launcher --ip=0.0.0.0 -f $connection_file
