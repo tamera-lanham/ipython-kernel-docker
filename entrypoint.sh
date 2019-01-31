43f6d7e52348091af1dddc8c0a0a0e8924cbd6e9
Matt Morgis (ELS-PHI) <m.morgis@elsevier.com>
2019-01-31 11:31:26 -0500

    feat: attempting to use tini to act as process subreaper

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ cat $connection_file
 # Modify the damn connection file to use proper IP address now
 sed -i 's;127.0.0.1;0.0.0.0;' ${connection_file}
 
-python3 -m ipykernel_launcher --ip=0.0.0.0 -f $connection_file
+/usr/bin/tini -- python3 -m ipykernel_launcher --ip=0.0.0.0 -f $connection_file
